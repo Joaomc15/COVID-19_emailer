ce1c69d9a4b4ac8c548069549ee6fb1f634e5e0b
Jo_ouch <56238440+Joaomc15@users.noreply.github.com>
2020-04-03 23:12:33 -0400

    new commit

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ import spreadsheet
 EMAIL_ADDRESS = "mdccovidupdates@gmail.com"
 PASSWORD = "EugeneLi"
 MAIL_LIST_Miami = []
-# MAIL_LIST_Miami = ['joaomcarvalhoedu@gmail.com', 'cmocelincarvalho@gmail.com']
-MAIL_LIST_Miami = spreadsheet.get_miami_emails()
+MAIL_LIST_Miami = ['joaomcarvalhoedu@gmail.com'] #, 'cmocelincarvalho@gmail.com']
+# MAIL_LIST_Miami = spreadsheet.get_miami_emails()
 MAIL_LIST_Broward = []
 MAIL_LIST_Broward = ['joaomcarvalhoedu@gmail.com']
 # MAIL_LIST_Broward = [spreadsheet.get_broward_emails()]
